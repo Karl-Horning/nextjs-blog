96b9f633fbf6fe02fe4fb3b816ff071aa40c7d1b
Karl Horning <32777806+Karl-Horning@users.noreply.github.com>
2024-03-14 23:13:45 +0000

    Add hero and reorganise homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
 import Navigation from "@/components/navigation";
+import Hero from "@/components/hero";
 
 export default function App() {
     return (
-        <main className="bg-gray-50 border-b-2 border-slate-200">
-            <Navigation />
-        </main>
+        <>
+            <div
+                id="nav-container"
+                className="bg-gray-50 border-b-2 border-slate-200"
+            >
+                <Navigation />
+            </div>
+            <main>
+                <Hero />
+            </main>
+        </>
     );
 }
