9921a0bde0dabe8ea371141ab7914f4735fdcd62
Karl Horning <32777806+Karl-Horning@users.noreply.github.com>
2024-03-18 22:08:16 +0000

    Update homepage layout with main and sidebar, add example trending post

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import LatestPosts from "@/components/latest-posts";
 import AllPosts from "@/components/all-posts";
 import Subscribe from "@/components/subscribe";
 import Footer from "@/components/footer";
+import Categories from "@/components/categories";
+import TrendingPosts from "@/components/trending-posts";
 
 export default function App() {
     return (
@@ -11,8 +13,18 @@ export default function App() {
             <Navigation />
             <Hero />
             <LatestPosts />
-            <AllPosts />
-            <Subscribe />
+            <div className="container mx-auto px-4">
+                <div className="grid md:grid-cols-3 gap-4">
+                    <main id="blog-posts" className="md:col-span-2">
+                        <AllPosts />
+                        <Subscribe />
+                    </main>
+                    <aside id="sidebar" className="md:col-span-1">
+                        <Categories />
+                        <TrendingPosts />
+                    </aside>
+                </div>
+            </div>
             <Footer />
         </main>
     );
