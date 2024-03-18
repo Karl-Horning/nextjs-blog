7a27514507554d059161be36b3b3655b1610fed1
Karl Horning <32777806+Karl-Horning@users.noreply.github.com>
2024-03-18 23:14:02 +0000

    Update example popular posts styles

--- src/components/trending-posts.tsx
+++ src/components/trending-posts.tsx
@@ -1,11 +1,11 @@
 import {
     Card,
     CardHeader,
-    CardBody,
     CardFooter,
-    Divider,
     Link,
     Image,
+    Button,
+    Avatar,
 } from "@nextui-org/react";
 
 export default function TrendingPosts() {
@@ -18,37 +18,102 @@ export default function TrendingPosts() {
             </div>
 
             <div className="px-6">
-                <Card className="max-w-[400px]">
-                    <CardHeader className="flex gap-3">
-                        <Image
-                            alt="nextui logo"
-                            height={100}
-                            radius="sm"
-                            src="https://avatars.githubusercontent.com/u/86160567?s=200&v=4"
-                            width={100}
-                        />
-                        <div className="flex flex-col">
-                            <p className="text-md">NextUI</p>
-                            <p className="text-small text-default-500">
-                                nextui.org
-                            </p>
-                        </div>
-                    </CardHeader>
-                    <Divider />
-                    <CardBody>
-                        <p>
-                            Make beautiful websites regardless of your design
-                            experience.
+                <Card isFooterBlurred className="w-full h-[300px] mb-2">
+                    <CardHeader className="absolute z-10 top-1 flex-col items-start">
+                        <p className="text-tiny text-white/60 uppercase font-bold">
+                            News
                         </p>
-                    </CardBody>
-                    <Divider />
-                    <CardFooter>
-                        <Link
-                            isExternal
-                            showAnchorIcon
-                            href="https://github.com/nextui-org/nextui"
-                        >
-                            Visit source code on GitHub.
+                        <h4 className="text-white font-medium text-2xl">
+                            Stream the Acme event
+                        </h4>
+                    </CardHeader>
+                    <Image
+                        removeWrapper
+                        alt="Card example background"
+                        className="z-0 w-full h-full scale-125 -translate-y-6 object-cover"
+                        src="/images/card-example-4.jpg"
+                    />
+                    <CardFooter className="absolute bg-white/30 bottom-0 border-t-1 border-zinc-100/50 z-10 justify-between">
+                        <div>
+                            <Avatar src="/images/150.jpg" />
+                            <p className="text-white text-tiny">By Karl</p>
+                        </div>
+                        <Link href="#">
+                            <Button
+                                className="text-tiny uppercase"
+                                color="danger"
+                                radius="full"
+                                size="md"
+                                fullWidth
+                            >
+                                Read more
+                            </Button>
+                        </Link>
+                    </CardFooter>
+                </Card>
+
+                <Card isFooterBlurred className="w-full h-[300px] mb-2">
+                    <CardHeader className="absolute z-10 top-1 flex-col items-start">
+                        <p className="text-tiny text-white/60 uppercase font-bold">
+                            Article
+                        </p>
+                        <h4 className="text-white font-medium text-2xl">
+                            Your checklist for better sleep
+                        </h4>
+                    </CardHeader>
+                    <Image
+                        removeWrapper
+                        alt="Card example background"
+                        className="z-0 w-full h-full scale-125 -translate-y-6 object-cover"
+                        src="/images/card-example-5.jpg"
+                    />
+                    <CardFooter className="absolute bg-white/30 bottom-0 border-t-1 border-zinc-100/50 z-10 justify-between">
+                        <div>
+                            <Avatar src="/images/150.jpg" />
+                            <p className="text-white text-tiny">By Karl</p>
+                        </div>
+                        <Link href="#">
+                            <Button
+                                className="text-tiny uppercase"
+                                color="danger"
+                                radius="full"
+                                size="md"
+                            >
+                                Read more
+                            </Button>
+                        </Link>
+                    </CardFooter>
+                </Card>
+
+                <Card isFooterBlurred className="w-full h-[300px] mb-2">
+                    <CardHeader className="absolute z-10 top-1 flex-col items-start">
+                        <p className="text-tiny text-white/60 uppercase font-bold">
+                            Review
+                        </p>
+                        <h4 className="text-black font-medium text-2xl">
+                            Acme camera
+                        </h4>
+                    </CardHeader>
+                    <Image
+                        removeWrapper
+                        alt="Card example background"
+                        className="z-0 w-full h-full scale-125 -translate-y-6 object-cover"
+                        src="/images/card-example-6.jpg"
+                    />
+                    <CardFooter className="absolute bg-white/30 bottom-0 border-t-1 border-zinc-100/50 z-10 justify-between">
+                        <div>
+                            <Avatar src="/images/150.jpg" />
+                            <p className="text-black text-tiny">By Karl</p>
+                        </div>
+                        <Link href="#">
+                            <Button
+                                className="text-tiny uppercase"
+                                color="danger"
+                                radius="full"
+                                size="md"
+                            >
+                                Read more
+                            </Button>
                         </Link>
                     </CardFooter>
                 </Card>
