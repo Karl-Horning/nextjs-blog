fdbdd38fee18fb2051be58b8f9d464990ed3ad7f
Karl Horning <32777806+Karl-Horning@users.noreply.github.com>
2024-03-21 09:12:11 +0000

    Update newsletter style

--- src/components/subscribe.tsx
+++ src/components/subscribe.tsx
@@ -2,40 +2,36 @@ import { Input, Button } from "@nextui-org/react";
 
 export default function Subscribe() {
     return (
-        <section id="newsletter" className="container mx-auto px-4 mb-16">
-            <div className="bg-blue-200 p-20 rounded-lg">
-                <div className="grid grid-cols-2 gap-4">
-                    <div>
-                        <h4 className="uppercase mb-2 text-slate-600">
-                            Newsletter
-                        </h4>
-                        <p className="mb-2 text-4xl text-blue-900">
-                            Subscribe to our newsletter to receive news and
-                            updates.
-                        </p>
-                        <p className="text-slate-600">
-                            Get the latest posts directly to your email every
-                            week!
-                        </p>
-                    </div>
+        <section id="newsletter" className="pb-16">
+            <div className="pb-10">
+                <h4 className="text-xl font-bold text-white bg-black py-4 px-6">
+                    Newsletter
+                </h4>
+            </div>
 
-                    <form action="">
-                        <Input
-                            isClearable
-                            isRequired
-                            className="max-w-xl"
-                            color="primary"
-                            description="We'll never share your email with anyone else."
-                            label="Your Email"
-                            size="lg"
-                            type="email"
-                        />
+            <div className="bg-black p-4">
+                <p className="text-white mb-4">
+                    Get the latest posts directly to your email every week!
+                </p>
+                <form action="">
+                    <Input
+                        isClearable
+                        isRequired
+                        className="max-w-xl"
+                        description="We'll never share your email with anyone else."
+                        label="Your Email"
+                        radius="none"
+                        type="email"
+                    />
 
-                        <Button className="w-full py-6" color="primary" size="lg">
-                            Subscribe
-                        </Button>
-                    </form>
-                </div>
+                    <Button
+                        className="w-full py-6 rounded-none"
+                        color="danger"
+                        size="lg"
+                    >
+                        Subscribe
+                    </Button>
+                </form>
             </div>
         </section>
     );
